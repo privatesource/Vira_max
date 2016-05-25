50141364275635d694c6ba369ebc5b4a83670f78
golem1381 <pa.alemiii81@gmail.com>
2016-05-25 16:04:11 +0430

    Update kkreply.lua

--- plugins/kkreply.lua
+++ plugins/kkreply.lua
@@ -1,22 +1,22 @@
 local function get_message_callback(extra, success, result)
   if result.to.type == 'channel' then
-    if our_id == result.from.id then
+    if our_id == result.from.peer_id then
       send_msg(extra, "you can\'t kick me :p", ok_cb, false)
     else
       local del=
-channel_del_user("chat#id"..result.to.id, "user#id"..result.from.id, ok_cb, false)
+channel_kick("channel#id"..result.to.id, "user#id"..result.from.peer_id, ok_cb, false)
       if del == false then
         send_msg(extra, "Kicking failed.", ok_cb, false)
         return
       end
     end
   else
-    send_msg(extra, "You're not in a group", ok_cb, false)
+    send_msg(extra, "You're not in a SuperGroup", ok_cb, false)
     return
   end
 end
 local function run(msg, matches)
-  if msg.text == "!kick" and msg.reply_id then
+  if msg.text == "!kick" and is_momod(msg) and msg.reply_id then
     msgr = get_message(msg.reply_id,get_message_callback, get_receiver(msg))
   end
 end
@@ -27,8 +27,6 @@ return {
    patterns = {
       "^!kick$"
    },
-   run = run,
-   moderated = true,
-   hide = true
+   run = run
    
 }

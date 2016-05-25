d56514a024a891edb12e95f4ebfdc7271b9818a9
golem1381 <pa.alemiii81@gmail.com>
2016-05-25 12:47:59 +0430

    Update helphtml.lua

--- plugins/helphtml.lua
+++ plugins/helphtml.lua
@@ -1,13 +1,13 @@
 do
 
 function run(msg, matches)
-if matches[1]:lower() == 'help' then
+if matches[1]:lower() == 'help>' then
    send_document(get_receiver(msg), "./data/help.html", ok_cb, false)
 end
 end
 return {
 patterns = {
-"^![help]$",
+"^[help>]$",
 
 },
 run = run

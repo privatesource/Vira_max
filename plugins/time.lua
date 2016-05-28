861dca55c90d334b0209ce11c043389016b2953e
golem1381 <pa.alemiii81@gmail.com>
2016-05-28 16:04:21 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -1,14 +1,15 @@
-function run(msg, matches)
-local url , res = http.request('http://api.gpmod.ir/time/')
-if res ~= 200 then return "No connection" end
-local jdat = json:decode(url)
-local text = '⌚ ساعت '..jdat.FAtime..' \n📅 امروز '..jdat.FAdate..' میباشد.\n    ----\n⏰ '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\nFire Bot'
-return text
+local function run(msg, matches)
+if matches[1] == 'ime' then
+    local cmd = io.popen('curl http://cruel-plus.ir/time.php')
+      local result = cmd:read('*all')
+    cmd:close()
+    return result
 end
+end
+
 return {
-  patterns = {
-"^زمان$",
-"^[/!]([Tt][iI][Mm][Ee])$" 
-}, 
-run = run 
+patterns = {
+"^[/#!][Tt](ime)$",
+},
+run = run
 }

fb8d953ea38f8c8cc919dc91ef07133eaaf0105f
golem1381 <pa.alemiii81@gmail.com>
2016-06-03 17:06:54 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -1636,7 +1636,7 @@ local function run(msg, matches)
 				return "no owner,ask admins in support groups to set owner for your SuperGroup"
 			end
 			savelog(msg.to.id, name_log.." ["..msg.from.id.."] used /owner")
-			return "SuperGroup owner is ["..group_owner..']'
+			return "👷🏻SuperGroup owner is "..msg.from.username..''
 		end
 
 		if matches[1] == "modlist" then

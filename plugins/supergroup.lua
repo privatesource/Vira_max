379dff9d6d7ad42216f1bfd1117f4c9b08f6669b
golem1381 <pa.alemiii81@gmail.com>
2016-06-03 17:23:21 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -39,7 +39,7 @@ local function check_member_super(cb_extra, success, result)
       end
       data[tostring(groups)][tostring(msg.to.id)] = msg.to.id
       save_data(_config.moderation.data, data)
-	  local text = 'SuperGroup has been added!'
+	  local text = '⚜SuperGroup has been added!⚜'
       return reply_msg(msg.id, text, ok_cb, false)
     end
   end
@@ -63,7 +63,7 @@ local function check_member_superrem(cb_extra, success, result)
       end
       data[tostring(groups)][tostring(msg.to.id)] = nil
       save_data(_config.moderation.data, data)
-	  local text = 'SuperGroup has been removed'
+	  local text = '❌SuperGroup has been removed❌'
       return reply_msg(msg.id, text, ok_cb, false)
     end
   end

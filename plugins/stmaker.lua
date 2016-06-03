769fa1abb88b0c468e4c2d3a5c032b4bfccd1e80
golem1381 <pa.alemiii81@gmail.com>
2016-06-03 13:34:04 +0430

    Update stmaker.lua

--- plugins/stmaker.lua
+++ plugins/stmaker.lua
@@ -1,65 +1,62 @@
- local function run(msg, matches)
-        local text = URL.escape(matches[1])
-        local color = 'blue'
-        if matches[2] == 'red' then
-            color = 'red'
-        elseif matches[2] == 'black' then
-            color = 'black'
-      elseif matches[2] == 'blue' then
-          color = 'blue'
-    elseif matches[2] == 'green' then
-        color = 'green'
-    elseif matches[2] == 'yellow' then
-        color = 'yellow'
-    elseif matches[2] == 'pink' then
-        color = 'magenta'
-    elseif matches[2] == 'orange' then
-        color = 'Orange'
-    elseif matches[2] == 'brown' then
-        color = 'DarkOrange'
+do
+local function run(msg, matches)
+  local lionteam = URL.escape(matches[1])
+  local size = '150'
+        if matches[2] == 'big' then
+            size = '400'
+        elseif matches[2] == 'normal' then
+            size = '100'
+      elseif matches[2] == '300' then
+          size = '300' 
+    elseif matches[2] == '200' then
+          size = '200'
+    elseif matches[2] == '100' then
+        size = '100'
+    elseif matches[2] == 'small' then
+        size = '50'
+    elseif matches[2] then
+        size = matches[2]
         end
-        local font = 'mathrm'
-        if matches[3] == 'bold' then
-            font = 'mathbf'
-        elseif matches[3] == 'italic' then
-            font = 'mathit'
-        elseif matches[3] == 'fun' then
-            font = 'mathfrak'
-        elseif matches[1] == 'arial' then
-            font = 'mathrm'
+  local f = 'ff2e4357'
+        if matches[3] == 'red' then
+            f = 'FF0000'
+        elseif matches[3] == 'blue' then
+            f = '002BFF'
+      elseif matches[3] == 'yellow' then
+          f = 'F7FF00' 
+    elseif matches[3] == 'black' then
+          f = '000000'
+    elseif matches[3] == 'orange' then
+          f = 'FF8000'
+    elseif matches[3] == 'lowblue' then
+        f = '00FFFB'
+    elseif matches[3] == 'purple' then
+        f = 'CB00FD'
+    elseif matches[3] == 'gray' then
+        f = '929C9A'
+    elseif matches[3] == 'green' then
+        f = '2AFF00' 
+    elseif matches[3] == 'pink' then
+        f = 'FF00E8'
+    elseif matches[3] then
+        f = matches[3]
         end
-        local size = '700'
-        if matches[4] == 'small' then 
-            size = '300'
-        elseif matches[4] == 'larg' then
-            size = '700'
-            end
-local url = 'http://latex.codecogs.com/png.latex?'..'\\dpi{'..size..'}%20\\huge%20\\'..font..'{{\\color{'..color..'}'..text..'}}'
-local file = download_to_file(url,'file.webp')
-if msg.to.type == 'channel' then
-send_document('channel#id'..msg.to.id,file,ok_cb,false)
-else
-send_document('chat#id'..msg.to.id,file,ok_cb,false)
+  local url = "http://assets.imgix.net/examples/redleaf.jpg?blur=120&w=700&h=400&fit=crop&txt="..lionteam.."&txtsize="..size.."&txtclr="..f.."&txtalign=middle,center&txtfont=Futura%20Condensed%20Medium&mono=ff6598cc=?markscale=60&markalign=center%2Cdown&mark64=aHR0cDovL2xpb250ZWFtLmlyL2ltZy9zcGVjdHJlbG9nby5wbmc"
+  local ext = ".webp"
+  local cb_extra = {file_path=file}
+
+  local receiver = get_receiver(msg)
+  local file = download_to_file(url, ".webp")
+  send_document(receiver, file, rmtmp_cb, cb_extra)
 end
-end
+
 return {
-   patterns = {
-       "^[/!#]sticker (.*) ([^%s]+) (.*) (small)$",
-       "^[/!#]sticker (.*) ([^%s]+) (.*) (larg)$",
-       "^[/!#]sticker (.*) ([^%s]+) (bold)$",
- "^[/!#]sticker (.*) (bold)$",
-       "^[/!#]sticker (.*) ([^%s]+) (italic)$",
-       "^[/!#]sticker (.*) ([^%s]+) (fun)$",
-       "^[/!#]sticker (.*) ([^%s]+) (arial)$",
-       "^[/!#]sticker (.*) (red)$",
-       "[/!#]sticker (.*) (black)$",
-       "^[/!#]sticker (.*) (blue)$",
-       "^[/!#]sticker (.*) (green)$",
-       "^[/!#]sticker (.*) (yellow)$",
-       "^[/!#]sticker (.*) (pink)$",
-       "^[/!#]sticker (.*) (orange)$",
-       "^[/!#]sticker (.*) (brown)$",
-       "^[/!#]sticker +(.*)$",
-       },
-   run = run
-} 
+  patterns = {
+    "^[/#!]sticker (.+) (.+) (.+)$",
+    "^[/#!]sticker (.+) (.+)$",
+    "^[/#!]sticker (.+)$"
+  },
+  run = run
+}
+
+end

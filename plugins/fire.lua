470ba054e3a9639afb159320b7bd26ee5b7ff5d3
komil <r.zasdf082@gmail.com>
2016-06-05 03:54:08 -0700

    Update fire.lua

--- plugins/fire.lua
+++ plugins/fire.lua
@@ -1,15 +1,15 @@
-do
-
-function run(msg, matches)
-   send_document(get_receiver(msg), "./data/sticker/animation.gif.mp4", ok_cb, false)
-end
-
-return {
-patterns = {
-"^![Ff]irebot$",
-
-},
-run = run
-}
-
-end
+--do
+--
+--function run(msg, matches)
+-- send_document(get_receiver(msg), "./data/sticker/animation.gif.mp4", ok_cb, false)
+--end
+--
+--return {
+--patterns = {
+--"^![Ff]irebot$",
+--
+--},
+--run = run
+--}
+--
+--end
